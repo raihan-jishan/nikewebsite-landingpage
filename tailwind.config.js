c13ffe54d6c3a111e7ec21323d3bc2eed3b4b469
raihan-jishan <raihanjishan3242@gmail.com>
2024-06-01 15:13:39 +0600

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,46 @@
+const { Palanquin, Montserrat } = require('next/font/google');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./Components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./sections/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    fontSize: {
+      xs: ["12px", "16px"],
+      sm: ["14px", "20px"],
+      base: ["16px", "19.5px"],
+      lg: ["18px", "21.94px"],
+      xl: ["20px", "24.38px"],
+      "2xl": ["24px", "29.26px"],
+      "3xl": ["28px", "50px"],
+      "4xl": ["48px", "58px"],
+      "8xl": ["96px", "106px"],
+    },
     extend: {
+      fontFamily: {
+        Palanquin: ["Palanquin", "sans-serif"],
+        Montserrat: ["Montserrat", "sans-serif"],
+      },
+      colors: {
+        primary: "#ECEEFF",
+        "coral-red": "#FF6452",
+        "slate-gray": "#6D6D6D",
+        "pale-blue": "#F5F6FF",
+        "white-400": "rgba(255, 255, 255, 0.80)",
+      },
+      boxShadow: {
+        "3xl": "0 10px 40px rgba(0, 0, 0, 0.1)",
+      },
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        card: "https://raw.githubusercontent.com/adrianhajdin/nike_landing_page/cfda8c5841b6e57f644f19aa10c4329553162a15/src/assets/images/thumbnail-background.svg",
+        hero: "https://raw.githubusercontent.com/adrianhajdin/nike_landing_page/cfda8c5841b6e57f644f19aa10c4329553162a15/src/assets/images/collection-background.svg",
+      },
+      screens: {
+        wide: "1440px",
       },
     },
   },
---
+++ utils/index.js
@@ -0,0 +1,25 @@
+import Button from "@/Components/Button";
+import Nav from "@/Components/Nav";
+import PopularProductCard from "@/Components/PopularProductCard";
+import ReviewCard from "@/Components/ReviewCard";
+import ServiceCard from "@/Components/ServiceCard";
+import ShoeCard from "@/Components/ShoeCard";
+import CustomerReviews from "@/sections/CustomerReviews";
+import Footer from "@/sections/Footer";
+import Hero from "@/sections/Hero";
+import PopularProducts from "@/sections/PopularProducts";
+import Services from "@/sections/Services";
+import SpecialOffer from "@/sections/SpecialOffer";
+import Subscribe from "@/sections/Subscribe";
+import SuperQuality from "@/sections/SuperQuality";
+
+export {
+    Button, CustomerReviews,
+    Footer,
+    Hero, Nav,
+    PopularProductCard, PopularProducts, ReviewCard,
+    ServiceCard, Services, ShoeCard, SpecialOffer,
+    Subscribe,
+    SuperQuality
+};
+
